99335f9ec35f4b55acc7d86c1213a79731460890
SophieDolan1 <87787846+SophieDolan1@users.noreply.github.com>
2021-07-26 11:53:22 +0930

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://sophiedolan1.github.io/my-project-for-class-sophie/](https://sophiedolan1.github.io/my-project-for-class-sophie/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/sophiedolan1/my-project-for-class-sophie** and import
+
+## Edit this project ![Build status badge](https://github.com/sophiedolan1/my-project-for-class-sophie/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/sophiedolan1/my-project-for-class-sophie** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/sophiedolan1/my-project-for-class-sophie/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,68 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . b b b b . . . . . . 
+        . . . . b b 3 3 3 3 b b . . . . 
+        . . . c b 3 3 3 3 1 1 b c . . . 
+        . . c b 3 3 3 3 3 1 1 1 b c . . 
+        . c b 1 1 1 3 3 3 3 1 1 3 c c . 
+        c b d 1 1 1 3 3 3 3 3 3 3 b b c 
+        c b b d 1 3 3 3 3 3 1 1 1 b b c 
+        c b b b 3 3 1 1 3 3 1 1 d d b c 
+        . c b b b d d 1 1 3 b d d d c . 
+        . . c c b b d d b b b b c c . . 
+        . . . . c c c c c c c c . . . . 
+        . . . . . b b d 1 1 b . . . . . 
+        . . . . . b d d 1 1 b . . . . . 
+        `, mySprite, 50, 0)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundColor(12)
+mySprite = sprites.create(img`
+    ........................
+    ............cc..........
+    ............ccc.........
+    ........ccc.ccccccc.....
+    ........ccccc555555cc...
+    ........ccb5555555555c..
+    .....ccc.b55555ff15555c.
+    .....cccb5555555ff55555c
+    ......cb555555555555d55c
+    ....c.b555555555bb55555c
+    ....ccb555ddd5555b13bbc.
+    ....ccd55ddddd555b3335c.
+    .....cdd5ddddddd55b335c.
+    ...c.bddddb555bbbd555c..
+    ...ccdddddbb55555bccc...
+    ...ccdddddddcc555bcc....
+    ...ccddddddddbcccbcccc..
+    .ccbddddddd55dbbbbc55c..
+    ccddddddddd555dbbcc5c...
+    cddddddbbbdd555bbccc....
+    .ccddddbbbbdd55bcc......
+    ...cccbbbbbdddbcddcc....
+    .....cccccdd555dcccc....
+    ..........cccccc........
+    `, SpriteKind.Projectile)
+controller.moveSprite(mySprite)
+let mySprite2 = sprites.create(img`
+    ...........fffffff...ccfff..........
+    ..........fbbbbbbbffcbbbbf..........
+    ..........fbb111bbbbbffbf...........
+    ..........fb11111ffbbbbff...........
+    ..........f1cccc1ffbbbbbcff.........
+    ..........ffc1c1c1bbcbcbcccf........
+    ...........fcc3331bbbcbcbcccf..ccccc
+    ............c333c1bbbcbcbccccfcddbbc
+    ............c333c1bbbbbbbcccccddbcc.
+    ............c333c11bbbbbccccccbbcc..
+    ...........cc331c11bbbbccccccfbccf..
+    ...........cc13c11cbbbcccccbbcfccf..
+    ...........c111111cbbbfdddddc.fbbcf.
+    ............cc1111fbdbbfdddc...fbbf.
+    ..............cccfffbdbbfcc.....fbbf
+    ....................fffff........fff
+    `, SpriteKind.Enemy)
